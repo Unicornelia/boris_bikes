d9077a563593b05f150ce4115a37e478279ed90b
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-08 16:36:14 +0000

    indents

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,12 @@
 require 'docking_station'
 
 describe "Docking Station" do
-it "should produce a new docking station object" do
-  expect(DockingStation.new).to be_kind_of(DockingStation)
-end
+  
+  it "should produce a new docking station object" do
+    expect(DockingStation.new).to be_kind_of(DockingStation)
+  end
 
-it "should call the method release bike on the object" do
-  expect(DockingStation.new).to respond_to(:release_bike)
-end
+  it "should call the method release bike on the object" do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
