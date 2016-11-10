0fa0e40bebe83aa16f2ed7552c050eac09d2ffe2
ejatkin <ejatkin@gmail.com>
2016-11-10 14:47:12 +0000

    Updated tests - working with Cornelia

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,20 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to "release_bike" }
-  it "creates an instance of class Bike, and checks it is working" do
-    bike = subject.release_bike
+
+  it { is_expected.to respond_to(:release_bike) }
+
+  it "gets a bike" do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+  end
+
+  it "checks it is working" do
+    bike = DockingStation.new.release_bike
     expect(bike).to be_working
   end
+
+#  it "docks bikes" do
+#    docked_bike = subject.dock(bike)
+#  end
+
 end
